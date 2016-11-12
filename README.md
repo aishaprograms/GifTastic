cd328d84f54cc55388592770108a5048d1c43512
Aisha <aishaprograms@gmail.com>
2016-11-12 08:41:22 -0600

    All commented and readme updated

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # GifTastic
 This is a website which uses AJAX to pull gifs from Giphy API when a specific search term is submitted. 
+New search terms can be added dynamically. When a button is clicked, the text of the button is used as the search term. A limit of 10 gifs are added to the page, with a rating of g or pg, and in a still state. When a user clicks the image, it becomes animated. When it is reclicked, it becomes still again.
+Heroku app: https://fathomless-lowlands-15510.herokuapp.com/
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,64 +1,4 @@
-$(document).ready(function() {
-    $('#add-animal').on('click', function() {
-        var searchTerm = $('#animal-input').val();
-        addAnimal(searchTerm);
-        renderButtons();
-        return false;
-    });
-    $('.close').click(function() {
-        $('.alert').hide();
-    });
-    renderButtons();
-
-    $(document).on('click', '.animal-btn', function() {
-        $('#animal-gifs').html('');
-        var queryURL = 'https://api.giphy.com/v1/gifs/search?';
-        var searchTerm = 'q=' + $(this).data('animal');
-        var limitSearch = '&limit=10';
-        var rating = '&rating=g&pg';
-        var apiKey = '&api_key=dc6zaTOxFJmzC';
-        queryURL = queryURL + searchTerm + limitSearch + rating + apiKey;
-
-        $.ajax({
-                url: queryURL,
-                method: 'GET'
-            })
-            .done(function(response) {
-                console.log(response);
-                var imagesArray = response.data;
-                for (var i = 0; i < imagesArray.length; i++) {
-                    var imgUrl = imagesArray[i].images.fixed_height_still.url;
-                    var animatedImgUrl = imagesArray[i].images.fixed_height.url;
-                    var imgDiv = $('<div>');
-                    imgDiv.attr('class', 'img-div');
-                    var rating = $('<p>');
-                    rating.text('Rating: ' + imagesArray[i].rating);
-                    var img = $('<img>');
-                    img.attr('src', imgUrl);
-                    img.attr('class', 'animal-img');
-                    img.attr('data-still', imgUrl);
-                    img.attr('data-animate', animatedImgUrl);
-                    img.attr('data-state', 'still');
-                    imgDiv.append(rating);
-                    imgDiv.append(img);
-                    $('#animal-gifs').prepend(imgDiv);
-                }
-            });
-    });
-
-    $(document).on('click', '.animal-img', function() {
-        var state = $(this).attr('data-state');
-        if (state == 'still') {
-            $(this).attr('src', $(this).data('animate'));
-            $(this).attr('data-state', 'animate');
-        } else {
-            $(this).attr('src', $(this).data('still'));
-            $(this).attr('data-state', 'still');
-        }
-    });
-
-});
-
+//global array of animals
 var animals = ['Budgie', 'Ragdoll Cat', 'Golden Retriever', 'African Gray Parrot',
     'Lamb', 'Cow', 'Chipmunk', 'Elephant', 'Hedgehog', 'Sloth', 'Red fox', 'Meerkat',
     'Hamster', 'Rabbit', 'Moose', 'Chicken', 'Octopus', 'Shark', 'Eagle'
@@ -91,3 +31,69 @@ function addButton(animalName) {
     button.text(animalName);
     $('#animal-buttons').append(button);
 }
+//When all DOM elements have loaded
+$(document).ready(function() {
+    //This part adds and animal to the array from which the buttons were created
+    $('#add-animal').on('click', function() {
+        var searchTerm = $('#animal-input').val();
+        addAnimal(searchTerm);
+        renderButtons();
+        return false;
+    });
+    // when this button is clicked, the alert warning, that a button with the search term has
+    // already been created, shows
+    $('#close-alert-button').click(function() {
+        $('.alert').hide();
+    });
+    renderButtons();
+    //when a dynamically generated button is clicked, all of the gifs matching it are shown
+    //this uses ajax calls and giphy api
+    //the gif images are added to the screen with their ratings
+    $(document).on('click', '.animal-btn', function() {
+        $('#animal-gifs').html('');
+        var queryURL = 'https://api.giphy.com/v1/gifs/search?';
+        var searchTerm = 'q=' + $(this).data('animal');
+        var limitSearch = '&limit=10';
+        var rating = '&rating=g&pg';
+        var apiKey = '&api_key=dc6zaTOxFJmzC';
+        queryURL = queryURL + searchTerm + limitSearch + rating + apiKey;
+
+        $.ajax({
+                url: queryURL,
+                method: 'GET'
+            })
+            .done(function(response) {
+                var imagesArray = response.data;
+                for (var i = 0; i < imagesArray.length; i++) {
+                    var imgUrl = imagesArray[i].images.fixed_height_still.url;
+                    var animatedImgUrl = imagesArray[i].images.fixed_height.url;
+                    var imgDiv = $('<div>');
+                    imgDiv.attr('class', 'img-div');
+                    var rating = $('<p>');
+                    rating.text('Rating: ' + imagesArray[i].rating);
+                    var img = $('<img>');
+                    img.attr('src', imgUrl);
+                    img.attr('class', 'animal-img');
+                    img.attr('data-still', imgUrl);
+                    img.attr('data-animate', animatedImgUrl);
+                    img.attr('data-state', 'still');
+                    imgDiv.append(rating);
+                    imgDiv.append(img);
+                    $('#animal-gifs').prepend(imgDiv);
+                }
+            });
+    });
+    //when an animal gif is clicked, the url pertaining to the animated or still state of
+    //the gif is added as a source url based on the current animated vs still state
+    $(document).on('click', '.animal-img', function() {
+        var state = $(this).attr('data-state');
+        if (state === 'still') {
+            $(this).attr('src', $(this).data('animate'));
+            $(this).attr('data-state', 'animate');
+        } else {
+            $(this).attr('src', $(this).data('still'));
+            $(this).attr('data-state', 'still');
+        }
+    });
+
+});
